d7cb18882545403fa49d5caed344bee544bd61ff
balintt21 <balintt21@gmail.com>
2018-08-21 13:39:13 +0200

    Added thread_sleep.h
    with sleep_for and sleep_until

---
+++ thread_sleep.h
@@ -0,0 +1,22 @@
+#ifndef __STD_THREAD_UTILS_H__
+#define __STD_THREAD_UTILS_H__
+
+#include <stdint.h>
+#include <chrono>
+#include <thread>
+
+namespace thread_utils
+{
+    inline void sleep_for(int64_t milliseconds)
+    {
+        std::this_thread::sleep_for();
+    }
+    
+    inline void sleep_until(int64_t timestamp_ms)
+    {
+        int64_t now = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
+        std::this_thread::sleep_for(timestamp_ms - now);
+    }
+}
+
+#endif
