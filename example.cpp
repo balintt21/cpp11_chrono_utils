7d68bfe09fdc09c0a4e5e170424836ccf6b4ea7e
balintt21 <balintt21@gmail.com>
2018-08-25 12:53:04 +0200

    Added example

---
+++ example.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "chrono_utils.h"
+#include "thread_sleep.h"
+
+
+int main(int argc, char** argv)
+{
+    puts("Test start");
+    printf("System time: \t%ld ms\nWindows time: \t%ld ms\nMacintosh time: %ld ms\nMonotonic time: %ld ms\n\n"
+        , chrono_utils::system_time_ms()
+        , chrono_utils::windows_time_ms()
+        , chrono_utils::mac_time_ms()
+        , chrono_utils::monotonic_time_ms());
+
+    auto t0 = chrono_utils::timediff_start();
+    thread_utils::sleep_for(2560);
+    printf("sleeped for %.4f sec\n", chrono_utils::timediff_s(t0));
+
+    auto t1 = chrono_utils::timediff_start();
+    int64_t now = chrono_utils::monotonic_time_ms();
+    int64_t deadline = now + 1560;
+    thread_utils::sleep_until(deadline);
+    printf("sleeped from %ld until %ld, duration: %ld sec\n", now, deadline, chrono_utils::timediff_ms(t1));
+
+    printf("total time spent: %.4f sec\n", chrono_utils::timediff_s(t0));
+    puts("Done.");
+
+    return 0;
+}
