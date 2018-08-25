63bd6e2579eeefd8fce8c47d51a4453f8d52780d
balintt21 <balintt21@gmail.com>
2018-08-25 12:51:57 +0200

    Update thread_sleep.h

--- thread_sleep.h
+++ thread_sleep.h
@@ -13,7 +13,8 @@ namespace thread_utils
      */
     inline void sleep_for(int64_t milliseconds)
     {
-        std::this_thread::sleep_for(milliseconds);
+        std::chrono::milliseconds ms{milliseconds};
+        std::this_thread::sleep_for(ms);
     }
     /*
      * Sleeps the current thread until the given monotonic time in milliseconds
@@ -22,7 +23,7 @@ namespace thread_utils
     inline void sleep_until(int64_t timestamp_ms)
     {
         int64_t now = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
-        std::this_thread::sleep_for(timestamp_ms - now);
+        sleep_for(timestamp_ms - now);
     }
 }
 
