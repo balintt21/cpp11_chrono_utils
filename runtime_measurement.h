35bdde0c2af630652e6739903d3f3a5e3cf5bac9
Balint Telkes <balintt21@gmail.com>
2018-10-19 12:36:21 +0200

    Update runtime_measurement.h

--- runtime_measurement.h
+++ runtime_measurement.h
@@ -15,14 +15,22 @@ class RuntimeMeasurement
 public:
     struct Result
     {
-        int64_t user_time_ns;
-        int64_t system_time_ns;
+        int64_t user_time_clktck;
+        int64_t system_time_clktck;
+        int64_t clocks_per_sec;
+
+        double userTimeToSec() const { return (user_time_clktck / static_cast<double>(clocks_per_sec)); }
+        double systemTimeToSec() const { return (system_time_clktck / static_cast<double>(clocks_per_sec)); }
     };
-    
+
+    static int64_t CLOCK_TICKS_PER_SEC() { return sysconf(_SC_CLK_TCK); }
+
     Result measure()
     {
         times(&mCurrentTimes);
-        return { (mCurrentTimes.tms_utime - mStartTimes.tms_utime), (mCurrentTimes.tms_stime - mStartTimes.tms_stime) };
+        return {  (mCurrentTimes.tms_utime - mStartTimes.tms_utime)
+                , (mCurrentTimes.tms_stime - mStartTimes.tms_stime)
+                , sysconf(_SC_CLK_TCK) };
     }
     
     RuntimeMeasurement(const std::function<void (int64_t, int64_t)>& result_callback = nullptr) : mResultCallback(result_callback)
@@ -35,7 +43,7 @@ public:
         if( mResultCallback )
         {
             auto measurement = measure();
-            mResultCallback(measurement.user_time_ns, measurement.system_time_ns);
+            mResultCallback(measurement.user_time_clktck, measurement.system_time_clktck);
         }
     }
 };
